bc990a0fcf9af4952e0b61247e62649facdf1b33
Candace Savonen <savonenc@msu.edu>
2018-10-29 13:53:42 -0400

    Fix directory references and other minor changes in 0-getdata

--- human_missingness/0-get_human_platform_info.R
+++ human_missingness/0-get_human_platform_info.R
@@ -9,7 +9,7 @@
 `%>%` <- dplyr::`%>%`
 
 # Read in the file:
-platforms <- read.csv("data/supported_microarray_platforms.csv")
+platforms <- read.csv(file.path("data", "supported_microarray_platforms.csv"))
 
 #------------------------ Get GEO list of human arrays-------------------------# 
 # Install and attach this package:
@@ -19,38 +19,40 @@ if (!("GEOmetadb" %in% installed.packages())) {
 }
 library(GEOmetadb)
 
+geo.db <- file.path("data", "GEOmetadb.sqlite")
+if (!file.exists(geo.db)) {
 # Check if the database is downloaded
-if(!file.exists('data/GEOmetadb.sqlite')){
   getSQLiteFile(destdir = "data")
 }
 
 # If it has been, connect to it
-if(file.exists('data/GEOmetadb.sqlite')){
-  con <- dbConnect(SQLite(),'data/GEOmetadb.sqlite')
+if(file.exists(geo.db)){
+  con <- dbConnect(SQLite(),geo.db)
 }
 # Get homo sapiens gpls
 hs.gpl <- dbGetQuery(con, paste0('SELECT gpl FROM gpl WHERE gpl.organism LIKE "%homo%"'))
 
 # Get info about these experiments
 hs.gpl <- geoConvert(hs.gpl$gpl, out_type = c("gse", "gpl", "gsm"),
-                     sqlite_db_name = "data/GEOmetadb.sqlite")
+                     sqlite_db_name = geo.db)
 
 # Save this info for future use
 save(hs.gpl, file = "GEO_exp_info.RData")
 
 #------------------Get ArrayExpress list of human arrays-----------------------# 
 # Read in the array express human experiments meta info
-hs.array <- read.table("data/array_express_human.txt", sep = "\t", skip = 1, stringsAsFactors = FALSE)
+hs.array <- read.table(file.path("data", "array_express_human.txt"), sep = "\t",
+                       skip = 1, stringsAsFactors = FALSE)
 
 #---------------Only keep supported platforms that are human-------------------#
 #
-# List of both GEO and array express accessions
+# Get a list of erxperiment accessions by combining the GEO and array acc lists
+# obtained from above. 
 hs.acc <- c(unique(hs.gpl$gse$from_acc), hs.array$V1)
 
 # Make dataframe with only human acc
 platforms <- platforms %>% dplyr::filter(external_accession %in% hs.acc)
 
 # Write info to a csv file
-write.csv(platforms, file = "data/exp_acc_human_only.csv", quote = FALSE,
-          row.names = FALSE)
-
+write.csv(platforms, file = file.path("data", "exp_acc_human_only.csv"),
+          quote = FALSE, row.names = FALSE)
